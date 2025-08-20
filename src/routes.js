f861dbd8a934d67a670f5e60e8c170001d1c1175
Emilly Paiva <emillypaiva3260@gmail.com>
2025-08-19 21:05:03 -0300

    feat: implementa o dashboard para visualização de casas do usuário

--- src/routes.js
+++ src/routes.js
@@ -3,17 +3,22 @@ import multer from 'multer';
 import uploadConfig from './config/upload'
 import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
+import DashboardController from './controllers/DashboardController';
+
 
 const routes = new Router();
 const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
 
-routes.post('/houses', upload.single('thumbnail'), HouseController.store)
+routes.post('/houses', upload.single('thumbnail'), HouseController.store);
 
-routes.get('/houses', HouseController.index)
+routes.get('/houses', HouseController.index);
 
-routes.put('/houses/:house_id', upload.single('thumbnail'),HouseController.update)
+routes.put('/houses/:house_id', upload.single('thumbnail'),HouseController.update);
 
-routes.delete('/houses', HouseController.destroy)
+routes.delete('/houses', HouseController.destroy);
+
+routes.get('/dashborad', DashboardController.show);
+
 export default routes;
